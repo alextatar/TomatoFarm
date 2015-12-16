85137aa0b3dd97cf1154cba8677e12bf14799ea2
alextatar <alextatar87@gmail.com>
2015-12-16 15:25:23 +0100

    Ad elapsed time in timer class

--- TomatoFarm.Tests/TimerTests.cs
+++ TomatoFarm.Tests/TimerTests.cs
@@ -20,6 +20,11 @@ namespace TomatoFarm.Tests
         private bool _wasExecuted;
         private int _times;
 
+        private void CheckElapsedTime(TimeSpan dueIn)
+        {
+            Math.Round(_timer.Elapsed.TotalSeconds).ShouldBeEquivalentTo(dueIn.TotalSeconds);
+        }
+
         [SetUp]
         public void Initialize()
         {
@@ -31,50 +36,76 @@ namespace TomatoFarm.Tests
         [Test]
         public void OneActionIsExecutedWhenCorespondingTimeElapses()
         {
-            _timer.Start(new TimedAction { Action = () => ExecuteAction(), DueIn = TimeSpan.FromSeconds(2) });
+            var dueIn = TimeSpan.FromSeconds(2);
+            _timer.Start(new TimedAction { Action = () => ExecuteAction(), DueIn = dueIn });
             Thread.Sleep(TimeSpan.FromSeconds(3.2));
 
             _wasExecuted.Should().BeTrue();
             _times.ShouldBeEquivalentTo(1);
+            CheckElapsedTime(dueIn);
         }
 
         [Test]
         public void EachActionIsExecutedWhenCorespondingTimeElapses()
         {
+            var dueIn = TimeSpan.FromSeconds(4);
             _timer.Start(
                 new TimedAction { Action = () => ExecuteAction(), DueIn = TimeSpan.FromSeconds(2) },
-                new TimedAction { Action = () => ExecuteAction(), DueIn = TimeSpan.FromSeconds(4) });
+                new TimedAction { Action = () => ExecuteAction(), DueIn = dueIn });
             Thread.Sleep(TimeSpan.FromSeconds(5));
 
             _wasExecuted.Should().BeTrue();
             _times.ShouldBeEquivalentTo(2);
+            CheckElapsedTime(dueIn);
         }
 
         [Test]
         public void GivenThatTimerIsStoppedNoActionExecuted()
         {
+            var stopAfter = TimeSpan.FromSeconds(1);
+
             _timer.Start(new TimedAction { Action = () => ExecuteAction(), DueIn = TimeSpan.FromSeconds(2) },
                 new TimedAction { Action = () => ExecuteAction(), DueIn = TimeSpan.FromSeconds(4) });
-            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Thread.Sleep(stopAfter);
             _timer.Stop();
             Thread.Sleep(TimeSpan.FromSeconds(5));
 
             _wasExecuted.Should().BeFalse();
             _times.ShouldBeEquivalentTo(0);
+            CheckElapsedTime(stopAfter);
         }
 
 
         [Test]
         public void DisposingTimerCancelsAlTasks()
         {
+            var stopAfter = TimeSpan.FromSeconds(1);
+
             _timer.Start(new TimedAction { Action = () => ExecuteAction(), DueIn = TimeSpan.FromSeconds(2) },
                 new TimedAction { Action = () => ExecuteAction(), DueIn = TimeSpan.FromSeconds(4) });
-            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Thread.Sleep(stopAfter);
             _timer.Dispose();
             Thread.Sleep(TimeSpan.FromSeconds(5));
 
             _wasExecuted.Should().BeFalse();
             _times.ShouldBeEquivalentTo(0);
+            CheckElapsedTime(stopAfter);
+        }
+
+        [Test]
+        public void StopWatchIsResetEveryWithEachStart()
+        {
+            var firstDueIn = TimeSpan.FromSeconds(2);
+            var secondDuedIn = TimeSpan.FromSeconds(4);
+
+            _timer.Start(new TimedAction { Action = () => ExecuteAction(), DueIn = firstDueIn });
+            Thread.Sleep(firstDueIn);
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            _timer.Start(new TimedAction { Action = () => ExecuteAction(), DueIn = secondDuedIn });
+            Thread.Sleep(secondDuedIn);
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+
+            CheckElapsedTime(secondDuedIn);
         }
 
         private void ExecuteAction()
--- TomatoFarm.Utils/TomatoTimer.cs
+++ TomatoFarm.Utils/TomatoTimer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,14 +10,28 @@ namespace TomatoFarm.Utils
     public class TomatoTimer : ITimer, IDisposable
     {
         private CancellationTokenSource _cancellationToken;
+        private Stopwatch _stopwatch; 
+
+        public TimeSpan Elapsed { get { return _stopwatch.Elapsed; } }
+
+        public TomatoTimer()
+        {
+            _stopwatch = new Stopwatch();
+        }
 
         public void Start(params TimedAction[] actions)
         {
             _cancellationToken = new CancellationTokenSource();
+            _stopwatch.Restart();
+            var longestAction = actions.Max(p => p.DueIn);
             foreach (var timedAction in actions)
             {
                 Task.Delay(timedAction.DueIn, _cancellationToken.Token)
-                    .ContinueWith((t, s) => timedAction.Action.Invoke(), null, CancellationToken.None,
+                    .ContinueWith((t, s) => {
+                        if (timedAction.DueIn == longestAction)
+                            _stopwatch.Stop();
+                        timedAction.Action.Invoke();
+                    }, null, CancellationToken.None,
                         TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion,
                         TaskScheduler.Default);
             }
@@ -25,11 +40,13 @@ namespace TomatoFarm.Utils
 
         public void Stop()
         {
+            _stopwatch.Stop();
             _cancellationToken.Cancel();
         }
 
         public void Dispose()
         {
+            _stopwatch.Stop();
             _cancellationToken.Cancel();
         }
     }
